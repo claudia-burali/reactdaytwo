eda830f264edd0d5235138c9687f47b4e9a66706
claudia-burali <161461840+claudia-burali@users.noreply.github.com>
2024-04-24 14:31:17 +0200

    mod

--- src/components/AllTheBooks.jsx
+++ src/components/AllTheBooks.jsx
@@ -1,13 +1,9 @@
 
 import Card from 'react-bootstrap/Card';
 import fantasy from "../data/fantasy.json";
-import history from "../data/history.json";
-import horror from "../data/horror.json";
-import romance from "../data/romance.json";
-import scifi from "../data/scifi.json";
 
-const AllTheBooks = () => {
-   fantasy.map((book) => {
+
+const AllTheBooks = () => { return (<>{fantasy.map((book) => {
       return (
         <Card style={{ width: "18rem" }} key={`book-${book.asin}`}>
           <Card.Img variant="top" src={book.img} />
@@ -15,48 +11,8 @@ const AllTheBooks = () => {
             <Card.Title>{book.title}</Card.Title>
           </Card.Body>
         </Card>
-      );
-    });
-    history.map((book) => {
-        return (
-          <Card style={{ width: "18rem" }} key={`book-${book.asin}`}>
-            <Card.Img variant="top" src={book.img} />
-            <Card.Body>
-              <Card.Title>{book.title}</Card.Title>
-            </Card.Body>
-          </Card>
-        );
-      });
-      horror.map((book) => {
-        return (
-          <Card style={{ width: "18rem" }} key={`book-${book.asin}`}>
-            <Card.Img variant="top" src={book.img} />
-            <Card.Body>
-              <Card.Title>{book.title}</Card.Title>
-            </Card.Body>
-          </Card>
-        );
-      });
-      romance.map((book) => {
-        return (
-          <Card style={{ width: "18rem" }} key={`book-${book.asin}`}>
-            <Card.Img variant="top" src={book.img} />
-            <Card.Body>
-              <Card.Title>{book.title}</Card.Title>
-            </Card.Body>
-          </Card>
-        );
-      });
-      scifi.map((book) => {
-        return (
-          <Card style={{ width: "18rem" }} key={`book-${book.asin}`}>
-            <Card.Img variant="top" src={book.img} />
-            <Card.Body>
-              <Card.Title>{book.title}</Card.Title>
-            </Card.Body>
-          </Card>
-        );
-      });
-  };
+      )
+    })}</>)}
+  
   
   export default AllTheBooks;
